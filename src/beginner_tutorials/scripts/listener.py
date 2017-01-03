5ac8ed78e6e1f0bcc64ae6ab351c9689327922ab
mynameisvinn <vin.tang@gmail.com>
2017-01-02 21:15:36 -0500

    add reference link

--- src/beginner_tutorials/scripts/listener.py
+++ src/beginner_tutorials/scripts/listener.py
@@ -5,7 +5,7 @@ original script from http://wiki.ros.org/ROS/Tutorials/WritingPublisherSubscribe
 
 original listener subscribed to "chatter" topic, which was published by a talker script. messages were of "string" type.
 
-
+refernece: http://answers.ros.org/question/223785/creating-a-subscriber-in-python/
 '''
 
 import rospy
@@ -15,6 +15,7 @@ from geometry_msgs.msg import Twist
 def callback(data):
     # changed data.data to data.linear.x - this is twist's data type
     # twist's attribute is linear.x
+    # http://answers.ros.org/question/223785/creating-a-subscriber-in-python/
     rospy.loginfo(rospy.get_caller_id() + 'I heard %s', data.linear.x)
 
 def listener():
