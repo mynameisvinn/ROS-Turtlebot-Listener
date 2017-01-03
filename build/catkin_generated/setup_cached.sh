2a36759468d4b5e9ee49def104aca667dda9e6e5
mynameisvinn <vin.tang@gmail.com>
2017-01-02 20:16:43 -0500

    initial commit

---
+++ build/catkin_generated/setup_cached.sh
@@ -0,0 +1,11 @@
+#!/usr/bin/env sh
+# generated from catkin/python/catkin/environment_cache.py
+
+# based on a snapshot of the environment before and after calling the setup script
+# it emulates the modifications of the setup script without recurring computations
+
+# new environment variables
+
+# modified environment variables
+export LD_LIBRARY_PATH="/home/ubuntu/catkin_ws/devel/lib:/opt/ros/indigo/lib:/usr/local/lib:/usr/local/cuda-6.5/lib:/usr/local/lib:/usr/local/lib"
+export PWD="/home/ubuntu/catkin_ws/build"
